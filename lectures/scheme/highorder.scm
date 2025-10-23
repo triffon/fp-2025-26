dfdbdf837cbcef43fd1bd18fa4ef65d40bbee63a
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2025-10-23 11:58:37 +0300

    Примерни функции, връщащи функции

--- lectures/scheme/highorder.scm
+++ lectures/scheme/highorder.scm
@@ -10,3 +10,14 @@
 (define (mystery x n) (accumulate + 0 0 n (lambda (i) (/ (pow x i) (fact i))) 1+))
 (define (exists? p? a b)
   (accumulate (lambda (x y) (or x y)) #f a b p? 1+))
+
+(define (square x) (* x x))
+(define (twice f) (lambda (x) (f (f x))))
+
+(define (derive f dx)
+  (lambda (x) (/ (- (f (+ x dx)) (f x)) dx)))
+
+(define (repeated f n)
+  (lambda (x)
+    (if (= n 0) x
+        (f ((repeated f (- n 1)) x)))))
