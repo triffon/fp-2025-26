bf44390e79d6533602cbee1d1274a5e87e15e2cd
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2025-11-04 10:50:57 +0200

    Помощни функции за първо контролно

---
+++ lectures/scheme/helpers.scm
@@ -0,0 +1,36 @@
+;; често използвани функции
+(define (id x) x)
+(define (1+ x) (+ x 1))
+(define (compose f g) (lambda (x) (f (g x))))
+
+;; натрупване от по-висок ред
+(define (accumulate op nv a b term next)
+  (if (> a b) nv
+      (op (term a) (accumulate op nv (next a) b term next))))
+
+;; функции от по-висок ред за списъци
+(define (filter p? l)
+  (cond ((null? l) l)
+        ((p? (car l)) (cons (car l) (filter p? (cdr l))))
+        (else (filter p? (cdr l)))))
+
+(define (foldr op nv l)
+  (if (null? l) nv
+      (op (car l) (foldr op nv (cdr l)))))
+
+(define (foldl op nv l)
+  (if (null? l) nv
+      (foldl op (op nv (car l)) (cdr l))))
+
+(define (foldr1 op l)
+  (if (null? (cdr l)) (car l)
+      (op (car l) (foldr1 op (cdr l)))))
+
+(define (foldl1 op l)
+  (foldl op (car l) (cdr l)))
+
+(define (search p l)
+  (and (not (null? l)) (or (p (car l)) (search p (cdr l)))))
+
+(define (all? p? l)
+  (not (search (lambda (x) (not (p? x))) l)))
