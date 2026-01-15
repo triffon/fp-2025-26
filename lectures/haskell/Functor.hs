9b34c82fe3fc5979abbccfaea1705f4c2a721abb
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2026-01-15 11:10:06 +0200

    Алтернативи

--- lectures/haskell/Functor.hs
+++ lectures/haskell/Functor.hs
@@ -215,3 +215,16 @@ sequenceA = foldr (liftA2 (:)) $ pure []
 
 -- >>> sequenceA [[1,2],[3,4],[5,6]]
 -- [[1,3,5],[1,3,6],[1,4,5],[1,4,6],[2,3,5],[2,3,6],[2,4,5],[2,4,6]]
+
+class Applicative f => Alternative f where
+   empty :: f a
+   (<|>) :: f a -> f a -> f a
+
+instance Alternative Maybe where
+   empty = Nothing
+   Just x  <|> _ = Just x
+   Nothing <|> y = y
+
+instance Alternative [] where
+  empty = []
+  (<|>) = (++)
